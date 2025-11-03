d81fb06fe514b3953d21decef06d8b0d82633211
skylarsoon <68980693+skylarsoon@users.noreply.github.com>
2025-11-02 21:10:50 -0800

    working radix theme +  added radix to buttons on main page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Theme } from "@radix-ui/themes";
 import "./index.css";               // Tailwind
-// import "@radix-ui/themes/styles.css"; // Radix Themes
+import "@radix-ui/themes/styles.css"; // Radix Themes
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
